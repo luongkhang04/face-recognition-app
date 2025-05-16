60ed3272e7a528edb13776cb253a245eaeb5e53f
Lương Thái Khang <luongkhang448@gmail.com>
2025-05-16 16:24:16 +0700

    disable debug

--- app.py
+++ app.py
@@ -109,4 +109,5 @@ def recognize():
     return jsonify({"name": "No face", "similarity": 0})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
